37330a5c6611dd97a337d701d121a41555288551
Dylan <119975251+CodeNameMeteor@users.noreply.github.com>
2026-01-19 13:03:07 +0000

    add comments to Account

--- src/main/java/ci553/happyshop/client/login/Account.java
+++ src/main/java/ci553/happyshop/client/login/Account.java
@@ -8,21 +8,26 @@ public abstract class Account {
 
     public Account(String username, String email, String pRawPassword, boolean isAlreadyHashed) {
         this.email = email;
+        //if the already hashed boolean is true use the inputted password and username as the hash
         if (isAlreadyHashed) {
             this.passwordHash = pRawPassword;
             this.usernameHash = username;
         } else {
+            //if not hash the inputted password and username and store them
             this.passwordHash = AccountUtils.hashString(pRawPassword);
             this.usernameHash = AccountUtils.hashString(username);
         }
     }
 
     public boolean checkLogin(String inputUser, String inputPassword) {
+        //check if the user hash and password hash are the same as the inputted ones
         return AccountUtils.checkHash(inputUser, this.usernameHash ) && AccountUtils.checkHash(inputPassword, this.passwordHash);
     }
 
+    //abstract function so the subclasses can write their own logic.
     public abstract String getAccountType();
 
+    //format account data into a simple string.
     public String toFileString() {
         return usernameHash + "," + email + "," + passwordHash + "," + getAccountType();
     }
