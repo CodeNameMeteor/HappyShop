b32793640b4c93c05085e8e8951a1b359f9de2f7
Dylan <119975251+CodeNameMeteor@users.noreply.github.com>
2026-01-19 12:51:14 +0000

    add comments to account utils

--- src/main/java/ci553/happyshop/client/login/AccountUtils.java
+++ src/main/java/ci553/happyshop/client/login/AccountUtils.java
@@ -6,23 +6,33 @@ import java.nio.charset.StandardCharsets;
 import java.math.BigInteger;
 
 public class AccountUtils {
-
+    //Hash's a given string.
     public static String hashString(String data) {
         try {
+            //Message Digest is a class inside of Java.Security
+            //that provides the use of various cryptographic functions.
             MessageDigest md = MessageDigest.getInstance("SHA-256");
+            //create a byte array of the hash digest bytes.
             byte[] hash = md.digest(data.getBytes(StandardCharsets.UTF_8));
+            //use big integer to convert the byte array into it's sign-magnitude representation
             BigInteger number = new BigInteger(1, hash);
+            //convert the hex representation into a string.
             StringBuilder hexString = new StringBuilder(number.toString(16));
+            //pad with leading zeros
             while (hexString.length() < 32) {
                 hexString.insert(0, '0');
             }
+            //return hex
             return hexString.toString();
+
         } catch (NoSuchAlgorithmException e) {
+            //catch if we specify a non-existent algorithm
             throw new RuntimeException(e);
         }
     }
 
     public static boolean checkHash(String inputPassword, String storedHash) {
+        //converts the inputted password into a hash and compare it to the stored hash.
         String inputHash = hashString(inputPassword);
         return inputHash.equals(storedHash);
     }
